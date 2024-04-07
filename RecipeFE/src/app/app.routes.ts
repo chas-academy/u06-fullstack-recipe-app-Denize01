6f75e3d23c3bc0194ddbc4a360746502b6fa1d78
Denize <denize.yildirim@chasacademy.se>
2024-04-07 14:29:36 +0000

    created a new home component, styled recipe hits, routes home to rootpage, removed content from index to apphtml, edited some minor thin gs in login and register form

--- RecipeFE/src/app/app.routes.ts
+++ RecipeFE/src/app/app.routes.ts
@@ -5,11 +5,14 @@ import { ProfileComponent } from './pages/profile/profile.component';
 import { authGuard } from './guard/auth.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'search', component: RecipesearchComponent },
   { path: 'recipe/:id', component: RecipeComponent },
   { path: 'profile', component: ProfileComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent },
 ];
